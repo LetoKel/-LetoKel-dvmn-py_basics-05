d711a9dbd99cc7cfcd8a0414bb6426eee6d96f52
LetoKel <letovgorsok@gmail.com>
2026-05-25 14:43:37 +0300

    Add files via upload

---
+++ lesson5.2.py
@@ -0,0 +1,140 @@
+import file_operations
+from faker import Faker
+import random
+import os
+
+
+SKILLS = [
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд",
+]
+
+RUNIC_ALPHABET = {
+    'а': 'а͠',
+    'б': 'б̋',
+    'в': 'в͒͠',
+    'г': 'г͒͠',
+    'д': 'д̋',
+    'е': 'е͠',
+    'ё': 'ё͒͠',
+    'ж': 'ж͒',
+    'з': 'з̋̋͠',
+    'и': 'и',
+    'й': 'й͒͠',
+    'к': 'к̋̋',
+    'л': 'л̋͠',
+    'м': 'м͒͠',
+    'н': 'н͒',
+    'о': 'о̋',
+    'п': 'п̋͠',
+    'р': 'р̋͠',
+    'с': 'с͒',
+    'т': 'т͒',
+    'у': 'у͒͠',
+    'ф': 'ф̋̋͠',
+    'х': 'х͒͠',
+    'ц': 'ц̋',
+    'ч': 'ч̋͠',
+    'ш': 'ш͒͠',
+    'щ': 'щ̋',
+    'ъ': 'ъ̋͠',
+    'ы': 'ы̋͠',
+    'ь': 'ь̋',
+    'э': 'э͒͠͠',
+    'ю': 'ю̋͠',
+    'я': 'я̋',
+    'А': 'А͠',
+    'Б': 'Б̋',
+    'В': 'В͒͠',
+    'Г': 'Г͒͠',
+    'Д': 'Д̋',
+    'Е': 'Е',
+    'Ё': 'Ё͒͠',
+    'Ж': 'Ж͒',
+    'З': 'З̋̋͠',
+    'И': 'И',
+    'Й': 'Й͒͠',
+    'К': 'К̋̋',
+    'Л': 'Л̋͠',
+    'М': 'М͒͠',
+    'Н': 'Н͒',
+    'О': 'О̋',
+    'П': 'П̋͠',
+    'Р': 'Р̋͠',
+    'С': 'С͒',
+    'Т': 'Т͒',
+    'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠',
+    'Х': 'Х͒͠',
+    'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠',
+    'Ш': 'Ш͒͠',
+    'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠',
+    'Ы': 'Ы̋͠',
+    'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠',
+    'Ю': 'Ю̋͠',
+    'Я': 'Я̋',
+    ' ': ' ',
+}
+
+RESULT_DIR = "result"
+NUMBER_OF_CHARACTERS = 10
+
+
+def generate_character_skills(skills_list, count=3):
+    return random.sample(skills_list, count)
+
+
+def convert_to_runic(text, alphabet):
+    runic_text = text
+    for original_letter, runic_letter in alphabet.items():
+        runic_text = runic_text.replace(original_letter, runic_letter)
+    return runic_text
+
+
+def create_character_context(fake):
+    character_skills = generate_character_skills(SKILLS, 3)
+    runic_skills = [convert_to_runic(skill, RUNIC_ALPHABET) for skill in character_skills]
+
+    return {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": runic_skills[0],
+        "skill_2": runic_skills[1],
+        "skill_3": runic_skills[2],
+    }
+
+
+def save_character_card(card_number, context):
+    os.makedirs(RESULT_DIR, mode=0o777, exist_ok=True)
+    result_card_name = os.path.join(RESULT_DIR, f"result{card_number}.svg")
+    file_operations.render_template("charsheet.svg", result_card_name, context)
+
+
+def main():
+    fake = Faker("ru_RU")
+
+    for number in range(1, NUMBER_OF_CHARACTERS + 1):
+        context = create_character_context(fake)
+        save_character_card(number, context)
+
+
+if __name__ == "__main__":
+    main()
+
+    
